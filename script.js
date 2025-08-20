8d34cade338f6a594c86a75f79f1c7ab00919b2c
muaramshoev mulkamon <muaramshoevvmulkamon@gmail.com>
2025-08-20 17:03:31 +0500

    my home work

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Данные о фильме
+const filmTitle = "Крестный отец";
+const director = "Фрэнсис Форд Коппола";
+const year = 1972;
 
+let isWatched = true;
+let rating = "Величайший фильм всех времен";
+
+// Вывод в консоль
+console.log("Название:", filmTitle, typeof filmTitle);
+console.log("Режиссер:", director, typeof director);
+console.log("Год:", year, typeof year);
+console.log("Статус:", isWatched ? "Фильм просмотрен" : "Фильм не просмотрен");
+console.log("Оценка:", rating, typeof rating);
